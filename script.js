f6abbd676f65cd30b730ac13b70b10bd41350890
DelphineLecorney <Lecorney.Delphine@gmail.com>
2023-05-16 22:11:11 +0200

    modify HTML and add select event listener

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-
 // Retrieve elements from the DOM
 const section = document.getElementsByClassName('section');
 const input = document.getElementById('name');
 const button = document.querySelector('.section-button');
+const select = document.getElementById('sort-select');
 const body = document.body;
 
 // Function to fetch name and handle response
-const fetchName = (name) => {
-  fetch("https://api.agify.io/?name=" + name)
+const fetchName = (name, countryId) => {
+  fetch(`https://api.agify.io/?name=${name}&country_id=${countryId}`)
     .then((response) => response.json())
     .then((data) => {
       section.innerHTML = ""; // Clear the body
@@ -26,4 +26,17 @@ const fetchName = (name) => {
 button.addEventListener('click', () => {
   let name = input.value;
   fetchName(name);
+});
+
+// Add select event listener
+select.addEventListener('change', () => {
+  let countryId = select.value;
+
+  if (countryId === 'France') {
+    fetchName('', 'FR');
+  } else if (countryId === 'Belgium') {
+    fetchName('', 'BE');
+  } else if (countryId === 'Italy') {
+    fetchName('', 'IT');
+  }
 });
